a36b82231d97233e60c8e95cc9447edc7c97acba
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-22 18:49:44 -0500

    fix: update basePath in Swagger configuration to root and ensure it reads directly from app.js

--- swagger.js
+++ swagger.js
@@ -5,15 +5,11 @@ const doc = {
     title: "Mi API de Inventario",
     description: "Documentación del Proyecto CRUD",
   },
-  host: "inventario-api-ypaj.onrender.com", // Tu host de Render
+  host: "inventario-api-ypaj.onrender.com",
   schemes: ["https"],
-  basePath: "/products", // <-- ¡CAMBIA ESTO A '/products'! Esto le mete el prefijo a todo.
 };
 
 const outputFile = "./swagger.json";
-
-// AQUÍ EL SEGUNDO CAMBIO: En vez de leer la carpeta routes,
-// haz que lea directamente desde app.js para que entienda el app.use('/products')
 const endpointsFiles = ["./app.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
