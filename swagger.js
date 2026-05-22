8986422d683023f093ea50b429c909ff074cee70
Elías Cabeza de Vaca <elways38@gmail.com>
2026-05-22 18:42:21 -0500

    fix: update Swagger configuration to read directly from app.js and adjust paths for products

--- swagger.js
+++ swagger.js
@@ -5,12 +5,15 @@ const doc = {
     title: "Mi API de Inventario",
     description: "Documentación del Proyecto CRUD",
   },
-  host: "inventario-api-ypaj.onrender.com",
+  host: "inventario-api-ypaj.onrender.com", // Tu host de Render
   schemes: ["https"],
-  basePath: "/products",
+  basePath: "/products", // <-- ¡CAMBIA ESTO A '/products'! Esto le mete el prefijo a todo.
 };
 
 const outputFile = "./swagger.json";
-const endpointsFiles = ["./routes/products.js"]; // Asegúrate que esta ruta apunte a tu archivo de rutas
+
+// AQUÍ EL SEGUNDO CAMBIO: En vez de leer la carpeta routes,
+// haz que lea directamente desde app.js para que entienda el app.use('/products')
+const endpointsFiles = ["./app.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
